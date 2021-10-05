8edba5955d67e88223693683b4718b848b08b223
johnpantini <johnpantini@github.com>
2021-10-05 18:08:00 +0300

    Make toast always visible in case of errors

---
+++ i18n/ru/validation.i18n.js
@@ -0,0 +1,13 @@
+i18n.extend({
+  applicationNotFound: 'Приложение %{appName} не найдено',
+  expiredToken: 'Срок действия токена истёк',
+  invalidMongoDBRealmKeys: 'Неверные ключи MongoDB Realm',
+  invalidToken: 'Неверный токен',
+  invalidTokenWithStatus: 'Неверный токен (код ошибки %{status})',
+  mongoDBProblemWithStatus: 'Проблема с MongoDB (код ошибки %{status})',
+  operationFailedWithStatus: 'Операция не выполнена (код ошибки %{status})',
+  operationDoneRefreshPage:
+    'Операция успешно выполнена, можно обновить страницу',
+  projectNotFound: 'Проект %{projectName} не найден',
+  userNotFound: 'Пользователь не найден'
+});
