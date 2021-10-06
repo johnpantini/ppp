46328bd89eb3b912d96d077c14b6d226a7fe8abf
johnpantini <johnpantini@github.com>
2021-10-06 22:13:56 +0300

    i18n fixes

---
+++ i18n/ru/base.i18n.js
@@ -0,0 +1,8 @@
+i18n.extend({
+  error: 'Ошибка',
+  help: 'Помощь',
+  loading: 'Загрузка',
+  save: 'Сохранить',
+  signOut: 'Выйти',
+  updates: 'Обновления'
+});
---
+++ i18n/ru/cloud-services.i18n.js
@@ -0,0 +1,14 @@
+i18n.extend({
+  cloudServices: {
+    toast: {
+      title: 'Настройка облачных сервисов',
+      auth0Step: 'Запись учётных данных в приложение Auth0',
+      gitHubStep: 'Сохранение данных авторизации Auth0 в GitHub',
+      mongoDBRealmStep: 'Настройка приложения MongoDB Realm'
+    }
+  },
+  personalGitHubToken: 'Персональный токен GitHub',
+  auth0EmailToken: 'Токен/Email Auth0',
+  mongoDBRealmPubKey: 'Публичный ключ MongoDB Realm',
+  mongoDBRealmPrivateKey: 'Приватный ключ MongoDB Realm'
+});
