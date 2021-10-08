a3ca097dec014c780572800db12c7867845e0823
johnpantini <johnpantini@github.com>
2021-10-08 23:09:06 +0300

    Add service machine field

--- desktop/cloud-services/cloud-services-page.js
+++ desktop/cloud-services/cloud-services-page.js
@@ -1,50 +1,19 @@
-/** @decorator */
-
-import { FoundationElement } from '../../lib/foundation-element/foundation-element.js';
-import { observable } from '../../lib/element/observation/observable.js';
+import { CloudServicesPage } from '../../base/cloud-services/cloud-services-page.js';
 import { ref } from '../../lib/element/templating/ref.js';
 import { html } from '../../lib/template.js';
 import { css } from '../../lib/element/styles/css.js';
 import { when } from '../../lib/element/templating/when.js';
-import { validate, invalidate } from '../../lib/validate.js';
+
 import {
   basePageStyles,
   circleSvg,
   loadingIndicator
 } from '../../design/leafygreen/styles/page.js';
 
+// TODO -theme
 import { settings } from '../../design/leafygreen/icons/settings.js';
 
-export class CloudServicesPage extends FoundationElement {
-  @observable
-  busy;
-
-  async saveCloudCredentials({ event }) {
-    try {
-      this.busy = true;
-
-      await validate(this.gitHubToken);
-
-      const r1 = await this.app.ppp.keyVault.checkGitHubToken({
-        token: this.gitHubToken.value
-      });
-
-      if (!r1.ok)
-        invalidate(this.gitHubToken, {
-          errorMessage: `Неверный токен (ошибка ${r1.status})`,
-          status: r1.status
-        });
-
-      this.app.ppp.keyVault.keys.gitHub = this.gitHubToken.value;
-
-      await validate(this.auth0Token);
-      // await validate(this.mongoPublicKey);
-      // await validate(this.mongoPrivateKey);
-    } finally {
-      this.busy = false;
-    }
-  }
-}
+await i18nImport(['cloud-services']);
 
 export const cloudServicesPageTemplate = (context, definition) => html`
   <template>
@@ -52,22 +21,23 @@ export const cloudServicesPageTemplate = (context, definition) => html`
     <form ${ref(
       'form'
     )} id="cloud-services" name="cloud-services" onsubmit="return false">
-      <div class="loading-wrapper" style="${(x) =>
-        x.busy ? 'opacity:.5' : 'opacity:1'}">
+      <div class="loading-wrapper" ?busy="${(x) => !!x.busy}">
         <section>
           <div class="section-index-icon">
             ${circleSvg(1)}
           </div>
           <div class="label-group">
-            <h6>Персональный токен GitHub</h6>
-            <p>Потребуется для автоматического обновления приложения PPP. <a
-              target="_blank"
-              href="https://pantini.gitbook.io/pantini-co/ppp/github">Посмотреть
-              инструкцию</a>.</p>
-            <${'ppp-text-field'}
-              placeholder="Токен"
-              name="github-token"
-              ${ref('gitHubToken')}
+            <h6>Сервисная машина</h6>
+            <p>Сервисная машина требуется для настройки компонентов PPP.
+              <a
+                target="_blank"
+                href="https://pantini.gitbook.io/pantini-co/ppp/service-machine">Посмотреть
+                инструкцию</a>.</p>
+            <ppp-text-field
+              placeholder="https://example.com"
+              name="service-machine-url"
+              value="${(x) => x.app.ppp.keyVault.getKey('service-machine-url')}"
+              ${ref('serviceMachineUrl')}
             ></ppp-text-field>
           </div>
         </section>
@@ -76,16 +46,29 @@ export const cloudServicesPageTemplate = (context, definition) => html`
             ${circleSvg(2)}
           </div>
           <div class="label-group">
-            <h6>Токен администрирования Auth0</h6>
+            <h6>${i18n.t('personalGitHubToken')}</h6>
+            <p>Потребуется для автоматического обновления приложения PPP. <a
+              target="_blank"
+              href="https://pantini.gitbook.io/pantini-co/ppp/github">Посмотреть
+              инструкцию</a>.</p>
+            <${'ppp-text-field'}
+              placeholder="Токен"
+              name="github-token"
+              value="${(x) => x.app.ppp.keyVault.getKey('github-token')}"
+              ${ref('gitHubToken')}
+            ></ppp-text-field>
+          </div>
+        </section>
+        <section>
+          <div class="section-index-icon">
+            ${circleSvg(3)}
+          </div>
+          <div class="label-group">
+            <h6>${i18n.t('auth0EmailToken')}</h6>
             <${'ppp-banner'} class="inline margin-top" appearance="warning">
               Токен Auth0
-              требуется только для первичной настройки приложения, он не
-              сохраняется. Если вы собираетесь использовать <a target="_blank"
-                                                               href="https://pantini.gitbook.io/pantini-co/ppp/cloudflare-pages">Cloudflare
-              Pages</a>,
-              вводите все настройки на этой странице, открыв её через ваш ресурс Cloudflare
-              Pages.
-            </ppp-banner>
+              требуется только на этапе настройки облачных сервисов, он не
+              сохраняется на сервере.
             </ppp-banner>
             <p>Сервис Auth0 служит для авторизации пользователей приложения PPP
               по
@@ -97,24 +80,19 @@ export const cloudServicesPageTemplate = (context, definition) => html`
             <ppp-text-field
               placeholder="Токен Auth0"
               name="auth0-token"
+              value="${(x) => x.app.ppp.keyVault.getKey('auth0-token')}"
               ${ref('auth0Token')}
             ></ppp-text-field>
-          </div>
-        </section>
-        <section>
-          <div class="section-index-icon">
-            ${circleSvg(3)}
-          </div>
-          <div class="label-group">
-            <h6>Публичный ключ MongoDB Realm</h6>
-            <p>MongoDB Realm обеспечивает приложение PPP хранилищем настроек и
-              платформой бессерверных функций. <a target="_blank"
-                                                  href="https://pantini.gitbook.io/pantini-co/ppp/mongodb">Посмотреть
-                инструкцию</a>.</p>
+            <p>Укажите Email пользователя, которого вы создали ранее в сервисе
+              Auth0 по <a
+                target="_blank"
+                href="https://pantini.gitbook.io/pantini-co/ppp/auth0">инструкции</a>.
+            </p>
             <ppp-text-field
-              placeholder="Публичный ключ"
-              name="mongo-public-key"
-              ${ref('mongoPublicKey')}
+              placeholder="Email пользователя Auth0"
+              name="auth0-email"
+              value="${(x) => x.app.ppp.keyVault.getKey('auth0-email')}"
+              ${ref('auth0Email')}
             ></ppp-text-field>
           </div>
         </section>
@@ -123,10 +101,29 @@ export const cloudServicesPageTemplate = (context, definition) => html`
             ${circleSvg(4)}
           </div>
           <div class="label-group">
-            <h6>Приватный ключ MongoDB Realm</h6>
+            <h6>${i18n.t('mongoDBRealmPubKey')}</h6>
+            <p>MongoDB Realm обеспечивает приложение PPP хранилищем настроек и
+              платформой бессерверных функций. <a target="_blank"
+                                                  href="https://pantini.gitbook.io/pantini-co/ppp/mongodb">Посмотреть
+                инструкцию</a>.</p>
+            <ppp-text-field
+              placeholder="Публичный ключ"
+              name="mongo-public-key"
+              value="${(x) => x.app.ppp.keyVault.getKey('mongo-public-key')}"
+              ${ref('mongoPublicKey')}
+            ></ppp-text-field>
+          </div>
+        </section>
+        <section>
+          <div class="section-index-icon">
+            ${circleSvg(5)}
+          </div>
+          <div class="label-group">
+            <h6>${i18n.t('mongoDBRealmPrivateKey')}</h6>
             <ppp-text-field
               placeholder="Приватный ключ"
               name="mongo-private-key"
+              value="${(x) => x.app.ppp.keyVault.getKey('mongo-private-key')}"
               ${ref('mongoPrivateKey')}
             ></ppp-text-field>
           </div>
@@ -138,7 +135,7 @@ export const cloudServicesPageTemplate = (context, definition) => html`
           <${'ppp-button'}
             ?disabled="${(x) => !!x.busy}"
             type="submit"
-            @click="${(x, c) => x.saveCloudCredentials(c)}"
+            @click="${(x) => x.saveCloudCredentials()}"
             appearance="primary"
           >
             ${when(
@@ -148,7 +145,7 @@ export const cloudServicesPageTemplate = (context, definition) => html`
                 cls: 'spinner-icon'
               })
             )}
-            Сохранить
+            ${i18n.t('save')}
           </ppp-button>
         </div>
       </section>
