1ad8ca2c64aa685521adb3f37a2c70b6990d3710
johnpantini <johnpantini@github.com>
2021-10-12 09:45:07 +0300

    Test chunked encoding

--- ppp-dyno.mjs
+++ ppp-dyno.mjs
@@ -1,13 +1,33 @@
-import { createServer } from 'http2';
+import { createServer } from 'http';
 
-createServer()
-  .on('error', (err) => console.error(err))
-  .on('stream', (stream, headers) => {
-    stream.respond({
-      ':status': 200
-    });
+let i = 0;
 
-    stream.write('Test unencrypted HTTP/2');
-    stream.end();
-  })
-  .listen(process.env.PORT ?? 3777);
+createServer((request, response) => {
+  response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+  response.setHeader('Transfer-Encoding', 'chunked');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  response.write(i + '');
+
+  const int = setInterval(() => {
+    console.log(1);
+
+    response.write(i++ + 'ABC');
+  }, 1000);
+
+  request.on('abort', () => {
+    console.log('abort');
+  });
+
+  request.on('error', () => {
+    clearInterval(int);
+  });
+
+  request.on('close', () => {
+    clearInterval(int);
+  });
+}).listen(process.env.PORT ?? 3777);
