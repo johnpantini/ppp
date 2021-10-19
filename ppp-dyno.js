fbb6ba0e3eb7855bf20427c82ce195aee83e7be9
johnpantini <johnpantini@github.com>
2021-10-19 13:59:09 +0300

    Add UT auth endpoint

--- ppp-dyno.js
+++ ppp-dyno.js
@@ -1,10 +1,107 @@
 const { createServer } = require('http');
+const { request } = require('https');
 const { Client } = require('./ppp-dyno/ssh2');
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
+
+async function fetch(url, options = {}) {
+  const u = new URL(url);
+
+  return new Promise(async (resolve, reject) => {
+    const requestOptions = {
+      hostname: u.hostname,
+      port: 443,
+      path: u.pathname,
+      method: options.method,
+      headers: options.headers
+    };
+
+    const req = request(requestOptions, async (res) => {
+      try {
+        let responseText = '';
+
+        for await (const chunk of res) {
+          responseText += chunk;
+        }
+
+        resolve({
+          responseText,
+          headers: res.headers,
+          statusCode: res.statusCode
+        });
+      } catch (error) {
+        reject({
+          responseText: error.toString(),
+          headers: res.headers,
+          statusCode: res.statusCode
+        });
+      }
+    });
+
+    req.on('error', (error) => reject(error));
+
+    if (options.body) req.write(options.body);
+
+    req.end();
+  });
+}
+
+async function ut(request, response) {
+  if (!/post/i.test(request.method)) {
+    return response.writeHead(405).end();
+  }
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    const body = JSON.parse(Buffer.concat(buffers).toString());
+    const utResponse = await fetch(
+      'https://sso.unitedtraders.com/rest/grpc/com.unitedtraders.luna.sessionservice.api.sso.SsoService.authorizeByFirstFactor',
+      {
+        method: 'POST',
+        headers: {
+          'User-Agent':
+            request.headers['user-agent'] ??
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          realm: 'aurora',
+          clientId: 'utcom',
+          loginOrEmail: body.login,
+          password: body.password,
+          product: 'UTCOM',
+          locale: 'ru'
+        })
+      }
+    );
+
+    response.setHeader('Content-Type', utResponse.headers['content-type']);
+    response.writeHead(utResponse.statusCode);
+    response.write(utResponse.responseText);
+    response.end();
+  } catch (e) {
+    console.error(e);
+
+    response.setHeader('Content-Type', 'application/json; charset=UTF-8');
+    response.writeHead(400);
+    response.write(
+      JSON.stringify({
+        error: {
+          message: e.message
+        }
+      })
+    );
+    response.end();
+  }
+}
 
 async function ssh(request, response) {
   if (!/post/i.test(request.method)) {
@@ -106,6 +203,8 @@ createServer((request, response) => {
       response.end();
 
       break;
+    case '/ut':
+      return ut(request, response);
     case '/ssh':
       return ssh(request, response);
     case '/ping':
