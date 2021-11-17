3a5295c7896b64cdfc437101ed5ea24e3b7f6a2a
johnpantini <johnpantini@github.com>
2021-11-17 22:54:12 +0300

    Remove unused code

--- desktop/not-found/not-found-page.js
+++ desktop/not-found/not-found-page.js
@@ -1,7 +1,6 @@
 /** @decorator */
 
 import { FoundationElement } from '../../lib/foundation-element/foundation-element.js';
-import { observable } from '../../lib/element/observation/observable.js';
 import { html } from '../../lib/template.js';
 import { css } from '../../lib/element/styles/css.js';
 import { notDefined } from '../../lib/utilities/style/display.js';
