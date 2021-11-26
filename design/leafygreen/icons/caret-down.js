2628b99cce889215bf3c01a18952c0a25f4065fa
johnpantini <johnpantini@github.com>
2021-11-26 23:12:54 +0300

    Add icons

---
+++ design/leafygreen/icons/caret-down.js
@@ -0,0 +1,12 @@
+import { svg } from './svg.js';
+
+export const caretDown = ({ size, role, slot, cls, viewBox }) =>
+  svg({
+    size,
+    role,
+    slot,
+    cls,
+    viewBox,
+    content:
+      '<path d="M8.679 10.796a.554.554 0 01-.858 0L4.64 6.976C4.32 6.594 4.582 6 5.069 6h6.362c.487 0 .748.594.43.976l-3.182 3.82z" fill="currentColor"></path>'
+  });
