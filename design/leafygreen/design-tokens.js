8604949ad918f31f438fcd3670876f42e0d739f7
johnpantini <johnpantini@github.com>
2021-11-26 23:13:17 +0300

    New tokens for height calculation

--- design/leafygreen/design-tokens.js
+++ design/leafygreen/design-tokens.js
@@ -6,3 +6,11 @@ const { create } = DesignToken;
 export const bodyFont = create('body-font').withDefault(
   '-apple-system, BlinkMacSystemFont, Roboto, Segoe UI, "Trebuchet MS", Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, Icons16, sans-serif'
 );
+/** @public */
+export const baseHeightMultiplier = create(
+  'base-height-multiplier'
+).withDefault(9);
+/** @public */
+export const density = create('density').withDefault(0);
+/** @public */
+export const designUnit = create('design-unit').withDefault(4);
