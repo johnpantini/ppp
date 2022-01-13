ea1fc23daf0b043e7676edc93875edf65ae84e83
johnpantini <johnpantini@github.com>
2022-01-14 00:59:47 +0300

    Remove deprecated crap

--- design/leafygreen/styles/size.js
+++
@@ -1,9 +0,0 @@
-import { cssPartial } from '../../../lib/element/styles/css.js';
-import { baseHeightMultiplier, density, designUnit } from '../design-tokens.js';
-
-/**
- * A formula to retrieve the control height.
- * Use this as the value of any CSS property that
- * accepts a pixel size.
- */
-export const heightNumber = cssPartial`(${baseHeightMultiplier} + ${density}) * ${designUnit}`;
--- design/leafygreen/terminal/terminal.js
+++
@@ -1,8 +0,0 @@
-import { Terminal, terminalTemplate } from '../../../lib/terminal/terminal.js';
-import { xtermStyles as terminalStyles } from '../styles/xterm.js';
-
-export const terminal = Terminal.compose({
-  baseName: 'terminal',
-  template: terminalTemplate,
-  styles: terminalStyles
-});
