18ce17c2e0a42545c15d18809051483c0d0ced6e
johnpantini <johnpantini@github.com>
2022-01-31 00:00:44 +0300

    Remove garbage

--- extensions/cmdist/desktop/leafygreen/pages/cmdist.js
+++ extensions/cmdist/desktop/leafygreen/pages/cmdist.js
@@ -5,7 +5,6 @@ import { css } from '[%#ctx.ppp.rootUrl%]/shared/element/styles/css.js';
 export const cmdistPageTemplate = (context, definition) => html`
   <template>
     <${'ppp-page-header'}>Cmdist (work in progress, not ready yet!)</ppp-page-header>
-    </div>
   </template>
 `;
 
