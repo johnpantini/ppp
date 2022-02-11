dbab5b58a149c931af8479faf10e748ee927b369
johnpantini <johnpantini@github.com>
2022-02-11 17:35:35 +0300

    Add endpoints page

---
+++ desktop/leafygreen/pages/endpoint.js
@@ -0,0 +1,119 @@
+import { EndpointPage } from '../../../shared/pages/endpoint.js';
+import { html } from '../../../shared/template.js';
+import { ref } from '../../../shared/element/templating/ref.js';
+import { when } from '../../../shared/element/templating/when.js';
+import { css } from '../../../shared/element/styles/css.js';
+import { pageStyles, loadingIndicator } from '../page.js';
+import { uuidv4 } from '../../../shared/ppp-crypto.js';
+
+const exampleCode = `exports = function({ query, headers, body}, response) {
+  return "OK";
+};`;
+
+export const endpointPageTemplate = (context, definition) => html`
+  <template>
+    <${'ppp-page-header'} ${ref('header')}>Конечная точка HTTPS
+    </ppp-page-header>
+    <form ${ref('form')} novalidate onsubmit="return false">
+      <div class="loading-wrapper" ?busy="${(x) => x.busy}">
+        <section>
+          <div class="label-group">
+            <h5>Маршрут</h5>
+            <p>Должен начинаться с /</p>
+          </div>
+          <div class="input-group">
+            <ppp-text-field
+              placeholder="/webhook"
+              value="${(x) => x.endpoint?.route}"
+              ${ref('route')}
+            ></ppp-text-field>
+            <${'ppp-button'}
+              class="margin-top"
+              @click="${(x) =>
+                (x.route.value = `/${uuidv4().replaceAll('-', '')}`)}"
+              appearance="primary"
+            >
+              Сгенерировать значение
+            </ppp-button>
+          </div>
+        </section>
+        <section>
+          <div class="label-group">
+            <h5>HTTP-метод</h5>
+          </div>
+          <div class="input-group">
+            <${'ppp-select'}
+              value="${(x) => x.endpoint?.method}"
+              ${ref('method')}
+            >
+              <ppp-option value="POST">POST</ppp-option>
+              <ppp-option value="GET">GET</ppp-option>
+              <ppp-option value="PUT">PUT</ppp-option>
+              <ppp-option value="DELETE">DELETE</ppp-option>
+              <ppp-option value="PATCH">PATCH</ppp-option>
+              <ppp-option value="*">Любой метод</ppp-option>
+            </ppp-select>
+          </div>
+        </section>
+        <section>
+          <div class="label-group">
+            <h5>Название функции</h5>
+            <p>Название облачной функции, которая будет обрабатывать запросы к
+              конечной точке.</p>
+          </div>
+          <div class="input-group">
+            <ppp-text-field
+              ?disabled="${(x) => x.endpoint?.function_name}"
+              placeholder="myWebhook"
+              value="${(x) => x.endpoint?.function_name}"
+              ${ref('functionName')}
+            ></ppp-text-field>
+          </div>
+        </section>
+        <section>
+          <div class="label-group full">
+            <h5>Функция</h5>
+            <p>Бизнес-логика обработки запроса на языке JavaScript.</p>
+            <${'ppp-codeflask'}
+              :code="${(x) => x.endpoint?.source ?? exampleCode}"
+              ${ref('source')}
+            ></ppp-codeflask>
+          </div>
+        </section>
+        ${when((x) => x.busy, html`${loadingIndicator()}`)}
+      </div>
+      <section class="last">
+        <div class="footer-actions">
+          <${'ppp-button'}
+            ?disabled="${(x) => x.busy}"
+            type="submit"
+            @click="${(x) => x.addEndpoint()}"
+            appearance="primary"
+          >
+            ${(x) =>
+              x.endpoint
+                ? 'Обновить конечную точну'
+                : 'Добавить конечную точку'}
+          </ppp-button>
+        </div>
+      </section>
+    </form>
+    </div>
+  </template>
+`;
+
+export const endpointPageStyles = (context, definition) =>
+  css`
+    ${pageStyles}
+    section ppp-codeflask {
+      width: 100%;
+      height: 256px;
+    }
+  `;
+
+// noinspection JSUnusedGlobalSymbols
+export const endpointPage = EndpointPage.compose({
+  baseName: 'endpoint-page',
+  template: endpointPageTemplate,
+  styles: endpointPageStyles
+});
---
+++ shared/pages/endpoint.js
@@ -0,0 +1,241 @@
+/** @decorator */
+
+import { BasePage } from '../page.js';
+import { validate } from '../validate.js';
+import { Observable, observable } from '../element/observation/observable.js';
+import { maybeFetchError } from '../fetch-error.js';
+import { Tmpl } from '../tmpl.js';
+
+export class EndpointPage extends BasePage {
+  @observable
+  endpoint;
+
+  processSource(source) {
+    const match = source.match(/\*\*@ppp([\s\S]+)@ppp\*/i);
+
+    if (match) return match[1].trim();
+    else return source;
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+
+    const endpointId = this.app.params()?.endpoint;
+
+    if (endpointId) {
+      this.beginOperation();
+
+      try {
+        const groupId = this.app.ppp.keyVault.getKey('mongo-group-id');
+        const appId = this.app.ppp.keyVault.getKey('mongo-app-id');
+        const token = await this.getMongoDBRealmAccessToken();
+
+        this.endpoint = await (
+          await fetch(
+            new URL(
+              'fetch',
+              this.app.ppp.keyVault.getKey('service-machine-url')
+            ).toString(),
+            {
+              cache: 'no-cache',
+              method: 'POST',
+              body: JSON.stringify({
+                method: 'GET',
+                url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/endpoints/${endpointId}`,
+                headers: {
+                  Authorization: `Bearer ${token}`
+                }
+              })
+            }
+          )
+        ).json();
+
+        if (!this.endpoint || this.endpoint?.error) {
+          this.failOperation(404);
+          await this.notFound();
+        } else {
+          const source = (
+            await (
+              await fetch(
+                new URL(
+                  'fetch',
+                  this.app.ppp.keyVault.getKey('service-machine-url')
+                ).toString(),
+                {
+                  cache: 'no-cache',
+                  method: 'POST',
+                  body: JSON.stringify({
+                    method: 'GET',
+                    url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/functions/${this.endpoint.function_id}`,
+                    headers: {
+                      Authorization: `Bearer ${token}`
+                    }
+                  })
+                }
+              )
+            ).json()
+          ).source;
+
+          this.endpoint.source = this.processSource(source);
+
+          Observable.notify(this, 'endpoint');
+        }
+      } catch (e) {
+        this.failOperation(e);
+      } finally {
+        this.endOperation();
+      }
+    }
+  }
+
+  async addEndpoint() {
+    this.beginOperation();
+
+    try {
+      await validate(this.route);
+      await validate(this.route, {
+        hook: async (value) => value.startsWith('/'),
+        errorMessage: 'Маршрут должен начинаться с /'
+      });
+      await validate(this.functionName);
+      await validate(this.source);
+
+      const groupId = this.app.ppp.keyVault.getKey('mongo-group-id');
+      const appId = this.app.ppp.keyVault.getKey('mongo-app-id');
+      const token = await this.getMongoDBRealmAccessToken();
+
+      const source = `/**@ppp\n${this.source.value.trim()}\n@ppp*/\n${await new Tmpl().render(
+        this,
+        this.source.value.trim(),
+        {}
+      )}`;
+
+      if (this.endpoint) {
+        const rUpdateFunction = await fetch(
+          new URL(
+            'fetch',
+            this.app.ppp.keyVault.getKey('service-machine-url')
+          ).toString(),
+          {
+            cache: 'no-cache',
+            method: 'POST',
+            body: JSON.stringify({
+              method: 'PUT',
+              url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/functions/${this.endpoint.function_id}`,
+              body: {
+                name: this.endpoint.function_name,
+                private: false,
+                source
+              },
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+        );
+
+        await maybeFetchError(rUpdateFunction);
+
+        const rUpdateEndpoint = await fetch(
+          new URL(
+            'fetch',
+            this.app.ppp.keyVault.getKey('service-machine-url')
+          ).toString(),
+          {
+            cache: 'no-cache',
+            method: 'POST',
+            body: JSON.stringify({
+              method: 'PUT',
+              url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/endpoints/${this.endpoint._id}`,
+              body: {
+                _id: this.endpoint._id,
+                route: this.route.value,
+                function_name: this.endpoint.function_name,
+                function_id: this.endpoint.function_id,
+                http_method: this.method.value,
+                validation_method: 'NO_VALIDATION',
+                secret_id: '',
+                secret_name: '',
+                create_user_on_auth: false,
+                fetch_custom_user_data: false,
+                respond_result: true,
+                last_modified: Math.floor(Date.now() * 1000),
+                disabled: false
+              },
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+        );
+
+        await maybeFetchError(rUpdateEndpoint);
+      } else {
+        const rNewFunction = await fetch(
+          new URL(
+            'fetch',
+            this.app.ppp.keyVault.getKey('service-machine-url')
+          ).toString(),
+          {
+            cache: 'no-cache',
+            method: 'POST',
+            body: JSON.stringify({
+              method: 'POST',
+              url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/functions`,
+              body: {
+                name: this.functionName.value.trim(),
+                private: false,
+                source
+              },
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+        );
+
+        await maybeFetchError(rNewFunction);
+
+        const jNewFunction = await rNewFunction.json();
+        const rNewEndpoint = await fetch(
+          new URL(
+            'fetch',
+            this.app.ppp.keyVault.getKey('service-machine-url')
+          ).toString(),
+          {
+            cache: 'no-cache',
+            method: 'POST',
+            body: JSON.stringify({
+              method: 'POST',
+              url: `https://realm.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/endpoints`,
+              body: {
+                route: this.route.value,
+                function_name: jNewFunction.name,
+                function_id: jNewFunction._id,
+                http_method: this.method.value,
+                validation_method: 'NO_VALIDATION',
+                secret_id: '',
+                secret_name: '',
+                create_user_on_auth: false,
+                fetch_custom_user_data: false,
+                respond_result: true,
+                disabled: false
+              },
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+        );
+
+        await maybeFetchError(rNewEndpoint);
+      }
+
+      this.succeedOperation();
+    } catch (e) {
+      this.failOperation(e);
+    } finally {
+      this.endOperation();
+    }
+  }
+}
