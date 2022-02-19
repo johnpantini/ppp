107a96d3e9dde88a564f74865c005365ed4e41b9
johnpantini <johnpantini@github.com>
2022-02-19 21:48:17 +0300

    Add node canvas

---
+++ salt/states/ppp/lib/canvas/lib/bindings.js
@@ -0,0 +1,7 @@
+module.exports = require('../' +
+  process.platform +
+  '-' +
+  process.arch +
+  '-' +
+  process.versions.modules +
+  '/canvas.node');
---
+++ salt/states/ppp/lib/canvas/lib/pattern.js
@@ -0,0 +1,11 @@
+/*!
+ * Canvas - CanvasPattern
+ * Copyright (c) 2010 LearnBoost <tj@learnboost.com>
+ * MIT Licensed
+ */
+
+const bindings = require('./bindings');
+const { DOMMatrix } = require('./DOMMatrix');
+
+bindings.CanvasPatternInit(DOMMatrix);
+module.exports = bindings.CanvasPattern;
