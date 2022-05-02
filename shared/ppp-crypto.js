6b6eebc70ec974465f62ef468f1fc94a3984bd1a
johnpantini <johnpantini@github.com>
2022-05-02 22:38:36 +0300

    Add sha256 function

--- shared/ppp-crypto.js
+++ shared/ppp-crypto.js
@@ -63,6 +63,14 @@ export function uuidv4() {
   return stringify(rnds);
 }
 
+export async function sha256(message) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
+
 export class PPPCrypto {
   #key;
 
