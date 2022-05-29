a888cf70e3c72a82168e2d31eed0d09ce7ede928
johnpantini <johnpantini@github.com>
2022-05-29 20:58:38 +0300

    Add fetch polyfill

---
+++ sql/ppp-fetch.sql
@@ -0,0 +1,30 @@
+drop function if exists ppp_fetch(url text, options json);
+create or replace function ppp_fetch(url text, options json)
+returns json as
+$$
+  let headersPairs = [];
+  const headers = options.headers || {};
+
+  for (const h of Object.keys(headers)) {
+    headersPairs.push(`http_header('${h}','${headers[h]}')`);
+  }
+
+  const headersString = `array[${headersPairs.join(',')}]`;
+  let contentType = headers['content-type'] || headers['Content-Type'] || 'null';
+
+  if (contentType !== 'null')
+    contentType = `'${contentType}'`;
+
+  let body = options.body || 'null';
+
+  if (body !== 'null')
+    body = `'${JSON.stringify(body)}'`;
+
+  const request = plv8.execute(`select http(('${options.method || "GET"}', '${url}', ${headersString}, ${contentType}, ${body})::http_request);`)[0].http;
+
+  return {
+    responseText: request.content,
+	  headers: request.headers,
+	  status: request.status
+  }
+$$ language plv8;
