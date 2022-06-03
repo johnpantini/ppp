f1679c16919da2e3c1d1bca4902dbcaf4c1948af
johnpantini <johnpantini@github.com>
2022-06-03 16:50:56 +0300

    Fix indentation

--- sql/spbex-halts/start.sql
+++ sql/spbex-halts/start.sql
@@ -8,11 +8,11 @@ $$ language plv8;
 create or replace function ppp_interval_[%#payload.serviceId%](duration interval default '60 seconds')
 returns bool as $$
 declare
-    end_time timestamptz := now() + duration;
+  end_time timestamptz := now() + duration;
 begin
   while now() < end_time loop
     perform from dblink('dbname=[%#payload.api.db%] port=[%#payload.api.port%] host=[%#payload.api.hostname%] user=[%#payload.api.user%] password=[%#payload.api.password%]', 'select ppp_perform_job_[%#payload.serviceId%]();') as (res text);
-  perform pg_sleep(1);
+    perform pg_sleep(1);
   end loop;
 
   return true;
