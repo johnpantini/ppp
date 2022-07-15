30a41e99e367e093925742bad341e2cd4e38496f
johnpantini <johnpantini@github.com>
2022-07-15 19:02:44 +0300

    Add tailscale Dockerfile for fly.io

--- tailscale/main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:${PORT} &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=tailscale-socks5
-echo Tailscale is now running
