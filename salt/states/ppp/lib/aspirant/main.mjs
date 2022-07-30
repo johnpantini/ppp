78520aea99c85264c9f83f7ec41f61cd99e87395
johnpantini <johnpantini@github.com>
2022-07-30 22:48:37 +0300

    Add fetch endpoint

--- salt/states/ppp/lib/aspirant/main.mjs
+++ salt/states/ppp/lib/aspirant/main.mjs
@@ -3,6 +3,64 @@ import { Worker } from 'node:worker_threads';
 import uWS from '../uWebSockets.js/uws.js';
 
 const PORT = process.env.PORT ?? 32456;
+const STATUS_TO_PHRASE = {
+  100: 'Continue',
+  101: 'Switching Protocols',
+  102: 'Processing',
+  200: 'OK',
+  201: 'Created',
+  202: 'Accepted',
+  203: 'Non Authoritative Information',
+  204: 'No Content',
+  205: 'Reset Content',
+  206: 'Partial Content',
+  207: 'Multi-Status',
+  300: 'Multiple Choices',
+  301: 'Moved Permanently',
+  302: 'Moved Temporarily',
+  303: 'See Other',
+  304: 'Not Modified',
+  305: 'Use Proxy',
+  307: 'Temporary Redirect',
+  308: 'Permanent Redirect',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  402: 'Payment Required',
+  403: 'Forbidden',
+  404: 'Not Found',
+  405: 'Method Not Allowed',
+  406: 'Not Acceptable',
+  407: 'Proxy Authentication Required',
+  408: 'Request Timeout',
+  409: 'Conflict',
+  410: 'Gone',
+  411: 'Length Required',
+  412: 'Precondition Failed',
+  413: 'Request Entity Too Large',
+  414: 'Request-URI Too Long',
+  415: 'Unsupported Media Type',
+  416: 'Requested Range Not Satisfiable',
+  417: 'Expectation Failed',
+  418: "I'm a teapot",
+  419: 'Insufficient Space on Resource',
+  420: 'Method Failure',
+  421: 'Misdirected Request',
+  422: 'Unprocessable Entity',
+  423: 'Locked',
+  424: 'Failed Dependency',
+  428: 'Precondition Required',
+  429: 'Too Many Requests',
+  431: 'Request Header Fields Too Large',
+  451: 'Unavailable For Legal Reasons',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway',
+  503: 'Service Unavailable',
+  504: 'Gateway Timeout',
+  505: 'HTTP Version Not Supported',
+  507: 'Insufficient Storage',
+  511: 'Network Authentication Required'
+};
 
 function readJSON(res, cb) {
   let buffer;
@@ -210,6 +268,33 @@ export default class Aspirant {
       })
       .get('/ping', async (res) => {
         res.writeHeader('Content-Type', 'text/plain;charset=UTF-8').end('pong');
+      })
+      .post('/fetch', async (res) => {
+        readJSON(res, async (body = {}) => {
+          try {
+            const fetchResponse = await fetch(body.url, {
+              method: body.method ?? 'GET',
+              headers: body.headers,
+              body: body.body
+            });
+
+            const ct = fetchResponse.headers.get('content-type');
+
+            res
+              .writeStatus(
+                `${fetchResponse.status} ${STATUS_TO_PHRASE[fetchResponse.status]}`
+              )
+              .writeHeader('Content-Type', ct)
+              .end(await fetchResponse.text());
+          } catch (e) {
+            console.error(e);
+
+            res
+              .writeStatus('500 Internal Server Error')
+              .writeHeader('Content-Type', 'text/plain;charset=UTF-8')
+              .end('500 Internal Server Error');
+          }
+        });
       })
       .listen(PORT, async (listenSocket) => {
         if (listenSocket) {
