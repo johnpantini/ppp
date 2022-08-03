e6a3818a8c267d2a9de69446f16aa145e8d7d314
johnpantini <johnpantini@github.com>
2022-08-03 22:36:49 +0300

    Add refs directive

--- shared/element/templating/ref.js
+++ shared/element/templating/ref.js
@@ -41,3 +41,43 @@ export function ref(propertyName) {
     propertyName
   );
 }
+
+export class RefsBehavior {
+  /**
+   * Creates an instance of RefsBehavior.
+   * @param target - The element to reference.
+   * @param propertyName - The name of the property to assign the reference to.
+   */
+  constructor(target, propertyName) {
+    this.target = target;
+    this.propertyName = propertyName;
+  }
+
+  /**
+   * Bind this behavior to the source.
+   * @param source - The source to bind to.
+   */
+  bind(source) {
+    if (typeof source[this.propertyName] === 'undefined')
+      source[this.propertyName] = [];
+
+    source[this.propertyName].push(this.target);
+  }
+
+  /**
+   * Unbinds this behavior from the source.
+   */
+  unbind() {}
+}
+
+/**
+ * @param propertyName - The name of the property to assign the reference to.
+ * @public
+ */
+export function refs(propertyName) {
+  return new AttachedBehaviorHTMLDirective(
+    'ppp-refs',
+    RefsBehavior,
+    propertyName
+  );
+}
