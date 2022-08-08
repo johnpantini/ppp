87537b4f3c3ef3f1a76e091694557a3a33038d1b
johnpantini <johnpantini@github.com>
2022-08-08 22:32:44 +0300

    Check for variable

--- salt/states/ppp/lib/aspirant/start.sh
+++ salt/states/ppp/lib/aspirant/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-echo 'Starting up tailscale...'
+if test -n "${TAILSCALE_AUTH_KEY-}"; then
+  echo 'Starting up tailscale...'
 
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-/app/tailscale up --hostname=${TAILSCALE_HOSTNAME:=ppp-aspirant} --authkey=${TAILSCALE_AUTH_KEY}
+  /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+  /app/tailscale up --hostname=${TAILSCALE_HOSTNAME:=ppp-aspirant} --authkey=${TAILSCALE_AUTH_KEY}
 
-echo 'Tailscale started.'
+  echo 'Tailscale started.'
+fi
+
 echo 'Starting up PPP Aspirant...'
 
 node --no-warnings --inspect=0.0.0.0:9229 /app/aspirant/main.mjs
--- tailscale-proxy/start.sh
+++ tailscale-proxy/start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo 'Starting up tailscale...'
+if test -n "${TAILSCALE_AUTH_KEY-}"; then
+  echo 'Starting up tailscale...'
 
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-/app/tailscale up --hostname=${TAILSCALE_HOSTNAME:=tailscale-proxy} --authkey=${TAILSCALE_AUTH_KEY}
+  /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+  /app/tailscale up --hostname=${TAILSCALE_HOSTNAME:=tailscale-proxy} --authkey=${TAILSCALE_AUTH_KEY}
 
-echo 'Tailscale started.'
+  echo 'Tailscale started.'
+fi
 
 node --no-warnings --inspect=0.0.0.0:9229 tailscale-proxy.mjs
