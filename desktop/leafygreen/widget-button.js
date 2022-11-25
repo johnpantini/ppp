213d8d9d8c555c062e3e2c2500f4bfec855aa926
johnpantini <johnpantini@github.com>
2022-11-25 21:35:35 +0300

    Add widget button

---
+++ desktop/leafygreen/widget-button.js
@@ -0,0 +1,79 @@
+/** @decorator */
+
+import { attr } from '../../shared/element/components/attributes.js';
+import { Button } from '../../shared/button.js';
+import { css } from '../../shared/element/styles/css.js';
+import { buttonTemplate, baseButtonStyles } from './button.js';
+import { appearanceBehavior } from '../../shared/utilities/behaviors.js';
+
+export const successWidgetButtonStyles = (context, definition) => css`
+  :host([appearance='success']) .control {
+    background-color: #0bb06d;
+  }
+
+  :host([appearance='success']) .control:hover,
+  :host([appearance='success']) .control:active {
+    background-color: #13c17b;
+  }
+`;
+
+export const dangerWidgetButtonStyles = (context, definition) => css`
+  :host([appearance='danger']) .control {
+    background-color: #d53645;
+  }
+
+  :host([appearance='danger']) .control:hover,
+  :host([appearance='danger']) .control:active {
+    background-color: #ec4756;
+  }
+`;
+
+export const widgetButtonStyles = (context, definition) =>
+  css`
+    ${baseButtonStyles(context, definition)}
+    .control {
+      width: 100%;
+      height: 32px;
+      font-size: 13px;
+      line-height: 32px;
+      border-radius: 4px;
+      color: #ffffff;
+    }
+  `.withBehaviors(
+    appearanceBehavior(
+      'success',
+      successWidgetButtonStyles(context, definition)
+    ),
+    appearanceBehavior('danger', dangerWidgetButtonStyles(context, definition))
+  );
+
+export class WidgetButton extends Button {
+  @attr
+  appearance;
+
+  appearanceChanged(oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.classList.add(newValue);
+      this.classList.remove(oldValue);
+    }
+  }
+
+  /**
+   * @internal
+   */
+  connectedCallback() {
+    super.connectedCallback();
+
+    if (!this.appearance) {
+      this.appearance = 'success';
+    }
+  }
+}
+
+export default WidgetButton.compose({
+  template: buttonTemplate,
+  styles: widgetButtonStyles,
+  shadowOptions: {
+    delegatesFocus: true
+  }
+});
