fa2ef6a65454b8d6b9f51c034dcf4cadf5967d52
johnpantini <johnpantini@github.com>
2022-11-25 21:26:01 +0300

    Add off-click element

---
+++ shared/off-click-element.js
@@ -0,0 +1,46 @@
+import { FoundationElement } from './foundation-element.js';
+
+const ELEMENTS = [];
+
+export class OffClickElement extends FoundationElement {
+  documentOffClickHandler() {}
+
+  documentKeydownHandler() {}
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    ELEMENTS.push(this);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    ELEMENTS.splice(ELEMENTS.indexOf(this), 1);
+  }
+}
+
+document.addEventListener(
+  'pointerdown',
+  (event) => {
+    ELEMENTS.forEach((e) => {
+      if (!event.composedPath().find((n) => n === e)) {
+        e?.documentOffClickHandler.call(e, event);
+      }
+    });
+  },
+  {
+    passive: true
+  }
+);
+document.addEventListener(
+  'keydown',
+  (event) => {
+    ELEMENTS.forEach((e) => {
+      e?.documentKeydownHandler.call(e, event);
+    });
+  },
+  {
+    passive: true
+  }
+);
