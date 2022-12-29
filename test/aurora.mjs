c7f62804ff087978de7b39234b41472c6f2f0908
johnpantini <johnpantini@github.com>
2022-12-29 14:46:50 +0300

    Add aurora wrapper test

---
+++ test/aurora.mjs
@@ -0,0 +1,27 @@
+import { WebSocket } from '../salt/states/ppp/lib/websocket/websocket.mjs';
+
+const client = new WebSocket('ws://localhost:24567');
+
+client.on('message', (data) => {
+  const payload = JSON.parse(data);
+
+  if (payload[0]?.msg === 'connected') {
+    client.send(
+      JSON.stringify({
+        action: 'auth',
+        key: process.env.KEY,
+        secret: process.env.SECRET
+      })
+    );
+  } else if (payload[0]?.msg === 'authenticated') {
+    client.send(
+      JSON.stringify({
+        action: 'subscribe',
+        trades: ['AAPL'],
+        quotes: ['AAPL']
+      })
+    );
+  } else {
+    console.log(payload);
+  }
+});
