f60a0119427973de9d4434578cb370ad0aa0ca8e
johnpantini <johnpantini@github.com>
2023-01-17 22:59:10 +0300

    Enable Tinkoff trader

--- desktop/leafygreen/trader-page.js
+++ desktop/leafygreen/trader-page.js
@@ -153,7 +153,6 @@ export const traderPageTemplate = (context, definition) => html`
             </ul>
           </div>
           <${'ppp-button'}
-            disabled
             slot="action"
             @click="${() =>
               ppp.app.navigate({
