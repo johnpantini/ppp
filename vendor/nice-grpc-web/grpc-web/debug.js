ed0078db27730884e38f114a01c804bf8727d2ed
johnpantini <johnpantini@github.com>
2023-01-18 22:36:56 +0300

    Remove console.debug

--- vendor/nice-grpc-web/grpc-web/debug.js
+++ vendor/nice-grpc-web/grpc-web/debug.js
@@ -1,7 +1,3 @@
 export function debug(...args) {
-  if (console.debug) {
-    console.debug.apply(null, args);
-  } else {
-    console.log.apply(null, args);
-  }
+  console.log.apply(null, args);
 }
