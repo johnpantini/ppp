8ac6b25dd5a7d627564a4029804b0f09019abe14
johnpantini <johnpantini@github.com>
2023-01-20 15:50:30 +0300

    Add details

--- shared/trading-error.js
+++ shared/trading-error.js
@@ -1,9 +1,10 @@
 import { CustomError } from './custom-error.js';
 
 export class TradingError extends CustomError {
-  constructor({ message } = {}) {
+  constructor({ message, details } = {}) {
     super(message);
 
     this.name = this.constructor.name;
+    this.details = details;
   }
 }
