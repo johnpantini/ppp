38fb69c2293f5e4d37cc458dfd2aa763cc51129e
johnpantini <johnpantini@github.com>
2023-01-25 23:59:50 +0300

    Add MOEX bonds

--- desktop/leafygreen/instruments-import-page.js
+++ desktop/leafygreen/instruments-import-page.js
@@ -94,6 +94,9 @@ export const instrumentsImportPageTemplate = (context, definition) => html`
                   <ppp-option value="moex-stocks">
                     Акции Московской биржи
                   </ppp-option>
+                  <ppp-option value="moex-bonds">
+                    Облигации Московской биржи
+                  </ppp-option>
                 </ppp-select>
                 <${'ppp-checkbox'}
                   style="margin-top: 8px"
