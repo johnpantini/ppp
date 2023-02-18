d5cfa70d5cf04acd0e06f97ddbe320818aa8d6a1
johnpantini <johnpantini@github.com>
2023-02-18 17:45:41 +0300

    Add settings page stub

---
+++ elements/pages/settings.js
@@ -0,0 +1,63 @@
+/** @decorator */
+
+import { html, css, ref } from '../../vendor/fast-element.min.js';
+import { Page, pageStyles } from '../page.js';
+import '../text-field.js';
+import '../button.js';
+import '../banner.js';
+import '../radio-group.js';
+
+export const settingsPageTemplate = html`
+  <template class="${(x) => x.generateClasses()}">
+    <ppp-loader></ppp-loader>
+    <form novalidate>
+      <ppp-page-header>Параметры</ppp-page-header>
+      <section>
+        <div class="label-group">
+          <h5>Оформление приложения</h5>
+          <p class="description">
+            Светлое, тёмное или по выбору операционной системы.
+          </p>
+          <ppp-banner class="inline" appearance="warning">
+            Оформление вступит в силу после сохранения изменений.
+          </ppp-banner>
+        </div>
+        <div class="input-group">
+          <ppp-radio-group
+            orientation="vertical"
+            value="${(x) => x.document.darkMode ?? '2'}"
+            ${ref('darkMode')}
+          >
+            <ppp-radio value="2">По выбору системы</ppp-radio>
+            <ppp-radio value="1">Тёмное</ppp-radio>
+            <ppp-radio value="0">Светлое</ppp-radio>
+          </ppp-radio-group>
+        </div>
+      </section>
+    </form>
+  </template>
+`;
+
+export const settingsPageStyles = css`
+  ${pageStyles}
+  ppp-banner {
+    margin-top: 10px;
+  }
+`;
+
+export class SettingsPage extends Page {
+  collection = 'app';
+
+  getDocumentId() {
+    return '@settings';
+  }
+
+  async read() {
+    return ppp.settings;
+  }
+}
+
+export default SettingsPage.compose({
+  template: settingsPageTemplate,
+  styles: settingsPageStyles
+}).define();
