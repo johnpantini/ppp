b5b4a75fdd35dd7db6611b9f56e9689230e495b9
johnpantini <johnpantini@github.com>
2023-06-20 20:11:54 +0300

    WIP - add missing slash

--- salt/states/ppp/lib/aspirant/etc/nginx/njs/api/v1.js
+++ salt/states/ppp/lib/aspirant/etc/nginx/njs/api/v1.js
@@ -141,7 +141,7 @@ async function startDeployedWorker(
             }
 
             {{- range nomadService .Name }}
-            proxy_pass http://{{ .Address }}:{{ .Port }};{{- end }}
+            proxy_pass http://{{ .Address }}:{{ .Port }}/;{{- end }}
             proxy_http_version 1.1;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection $connection_upgrade;
