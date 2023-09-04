7029dbbaa8620173169c066b16402566888a78b9
johnpantini <johnpantini@github.com>
2023-09-04 15:48:30 +0300

    v4.7.1 - Bump nginx version

--- elements/pages/service-supabase-parser.js
+++ elements/pages/service-supabase-parser.js
@@ -42,26 +42,9 @@ description text not null,
 pub_date text not null,
 link text not null`,
     constsCode: `return [];`,
-    parsingCode: `// ==PPPScript==
-// @version 1
-// ==/PPPScript==
-
-const url = '[%#ctx.document.url || 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664'%]';
-const fetch = plv8.find_function('ppp_fetch');
-const rss = plv8.find_function('ppp_xml_parse')(fetch(url, {
-  headers: {
-    'User-Agent': '[%#navigator.userAgent%]'
-  }
-}).responseText);
-
-return (rss.rss.channel.item || []).map(item => {
-  return {
-    title: item.title,
-    description: item.description,
-    pub_date: item.pubDate || item['dc:date'] || new Date().toISOString(),
-    link: item.link
-  };
-}).sort((a, b) => Date.parse(a.pub_date) - Date.parse(b.pub_date));`,
+    parsingCode: await (
+      await fetch(`${ppp.rootUrl}/lib/supabase-parser/default-parser.js`)
+    ).text(),
     insertTriggerCode: `/**
  * @constant {string} TABLE_NAME - Имя таблицы состояния.
  */
